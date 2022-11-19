c96c887cdc10f371c61f5833c98979ea91eac66d
ajay kumar  shah <110763605+ajaykumarshah@users.noreply.github.com>
2022-11-19 14:47:55 +0530

    last

--- src/components/App.js
+++ src/components/App.js
@@ -5,24 +5,28 @@ import '../styles/App.css';
 
 class App extends React.Component {
 
-  handleClick(){
-    this.setState({count: this.state.count+1})
+
+
+  handleClick() {
+    this.setState({ count: this.state.count + 1 })
   }
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state = {count:0}
+    this.state = { count: 0 }
     this.handleClick = this.handleClick.bind(this);
 
   }
 
+  componentDidMount() {
+    console.log("mounted");
+  }
 
-  
   render() {
     console.log('rendered')
     return (
       <div id="main">
-          <div id="count">{this.state.count}</div>
-          <button id="incr" onClick={this.handleClick}>Increment</button>
+        <div id="count">{this.state.count}</div>
+        <button id="incr" onClick={this.handleClick}>Increment</button>
       </div>
     )
   }
